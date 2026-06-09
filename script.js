a58abfaa77e01abc015300f8569b155ac7d42a1b
BigMacMogger <hdhejaxowi@gmail.com>
2026-06-09 18:57:20 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
 alert("Script Loaded");
 
-document.getElementById("googleLoginBtn")
-.addEventListener("click", () => {
+const provider = new firebase.auth.GoogleAuthProvider();
 
-  alert("Button Works");
+document.getElementById("googleLoginBtn")
+.addEventListener("click", async () => {
+
+  try {
+
+    const result = await auth.signInWithPopup(provider);
+
+    const user = result.user;
+
+    alert("SUCCESS: " + user.email);
+
+    document.getElementById("loginScreen").classList.add("hidden");
+    document.getElementById("mainMenu").classList.remove("hidden");
+
+    document.getElementById("profileUsername").innerText =
+      user.displayName || user.email;
+
+  } catch (error) {
+
+    alert("ERROR: " + error.message);
+    console.error(error);
+
+  }
 
 });
