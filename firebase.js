9a3605e82d63b546144fc461a1b973ea4fe4d79f
BigMacMogger <hdhejaxowi@gmail.com>
2026-06-09 18:53:54 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,15 +1,17 @@
 const firebaseConfig = {
   apiKey: "AIzaSyDgJSJYCqQXzDhs7_VOWz13sobzoYNL7ao",
   authDomain: "omoogle-d0faa.firebaseapp.com",
+  databaseURL: "https://omoogle-d0faa-default-rtdb.firebaseio.com",
   projectId: "omoogle-d0faa",
-  storageBucket: "omoogle-d0faa.appspot.com",
+  storageBucket: "omoogle-d0faa.firebasestorage.app",
   messagingSenderId: "291641969937",
-  appId: "1:291641969937:web:b01f5c40857a3980da90b0",
-  databaseURL: "https://omoogle-d0faa-default-rtdb.firebaseio.com"
+  appId: "1:291641969937:web:8d8626af2021ceebda90b0"
 };
 
 firebase.initializeApp(firebaseConfig);
 
 const auth = firebase.auth();
 const db = firebase.database();
-    
+
+console.log("Firebase Loaded");
+alert("Firebase Loaded");
