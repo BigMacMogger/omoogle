52c9ec5c263d35a5f7893acf917d334c5a00710c
BigMacMogger <hdhejaxowi@gmail.com>
2026-06-09 19:00:42 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,13 @@
 alert("Script Loaded");
 
-const provider = new firebase.auth.GoogleAuthProvider();
+document.getElementById("loginScreen").style.display = "none";
+document.getElementById("mainMenu").style.display = "block";
 
-document.getElementById("googleLoginBtn")
-.addEventListener("click", async () => {
+document.getElementById("profileUsername").innerText =
+  "TEST USER";
 
-  try {
+document.getElementById("profileRank").innerText =
+  "Fed";
 
-    const result = await auth.signInWithPopup(provider);
-
-    const user = result.user;
-
-    alert("SUCCESS: " + user.email);
-
-    document.getElementById("loginScreen").classList.add("hidden");
-    document.getElementById("mainMenu").classList.remove("hidden");
-
-    document.getElementById("profileUsername").innerText =
-      user.displayName || user.email;
-
-  } catch (error) {
-
-    alert("ERROR: " + error.message);
-    console.error(error);
-
-  }
-
-});
+document.getElementById("profileElo").innerText =
+  "1000 ELO";
