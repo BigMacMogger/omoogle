40e72057d6390ada167c8c05b0c079dedd718aeb
BigMacMogger <hdhejaxowi@gmail.com>
2026-06-09 18:39:05 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,16 +1,15 @@
-alert("SCRIPT STARTED");
+const firebaseConfig = {
+  apiKey: "AIzaSyDgJSJYCqQXzDhs7_VOWz13sobzoYNL7ao",
+  authDomain: "omoogle-d0faa.firebaseapp.com",
+  projectId: "omoogle-d0faa",
+  storageBucket: "omoogle-d0faa.appspot.com",
+  messagingSenderId: "291641969937",
+  appId: "1:291641969937:web:b01f5c40857a3980da90b0",
+  databaseURL: "https://omoogle-d0faa-default-rtdb.firebaseio.com"
+};
 
-const provider = new firebase.auth.GoogleAuthProvider();
+firebase.initializeApp(firebaseConfig);
 
-document.getElementById("googleLoginBtn").addEventListener("click", () => {
-  alert("BUTTON CLICKED");
-
-  auth.signInWithPopup(provider)
-    .then((result) => {
-      alert("LOGGED IN: " + result.user.email);
-    })
-    .catch((error) => {
-      alert("ERROR: " + error.message);
-      console.error(error);
-    });
-});
+const auth = firebase.auth();
+const db = firebase.database();
+    
